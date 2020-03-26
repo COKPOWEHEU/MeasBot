850aba084d8b112d327ecb2f9fb0663652e501f4
petrolax <poa21y@mail.com>
2020-03-26 13:33:53 +0300

    e24: added function of init

--- modules/e24/e24.cpp
+++ modules/e24/e24.cpp
@@ -1,11 +1,25 @@
 #include <lua.hpp>
 #include <iostream>
+#include <string>
 #include <fstream>
 
-static int e24_init(lua_State *L) {
+std::fstream fs;
 
+static int e24_init(lua_State *L) {
+  std::string filename = luaL_checkstring(L, -1);
+  fs.open(filename.c_str(), std::fstream::out);
+  if(!fs.is_open()) {
+    lua_pushinteger(L, 0);
+    return 1;
+  }
+
+  lua_pushinteger(L, 1);
+  return 1;
 }
 
 static int e24_read(lua_State *L) {
+  
+}
 
+static int e24_close(lua_State *L) {
 }
