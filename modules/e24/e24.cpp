5bb12c9d4534f4180ffde358fef1750b0d8c0f2b
petrolax <poa21y@mail.com>
2020-03-26 14:07:32 +0300

    e24: added function of read from file

--- modules/e24/e24.cpp
+++ modules/e24/e24.cpp
@@ -7,7 +7,7 @@ std::fstream fs;
 
 static int e24_init(lua_State *L) {
   std::string filename = luaL_checkstring(L, -1);
-  fs.open(filename.c_str(), std::fstream::out);
+  fs.open(filename.c_str(), std::fstream::in);
   if(!fs.is_open()) {
     lua_pushinteger(L, 0);
     return 1;
@@ -18,7 +18,24 @@ static int e24_init(lua_State *L) {
 }
 
 static int e24_read(lua_State *L) {
-  
+  if(!fs.is_open()) {
+    lua_pushinteger(L, 0);
+    return 1;
+  }
+
+  double a, b;
+  static double prevB = 0;
+  fs >> a >> b;
+  if(prevB+0.5 < b) {
+    lua_pushnumber(L, a);
+    lua_pushnumber(L, prevB);
+    return 2;
+  }
+
+  prevB = b;
+  lua_pushnumber(L, a);
+  lua_pushnumber(L, b);
+  return 2;
 }
 
 static int e24_close(lua_State *L) {
