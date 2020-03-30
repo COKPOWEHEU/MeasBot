c1a5279b0def25cab4e8718ccf8b9b29ffd4776c
petrolax <poa21y@gmail.com>
2020-03-31 01:28:30 +0300

    e24: Added method readADC()

--- data/script_e24.lua
+++ data/script_e24.lua
@@ -4,3 +4,5 @@ package.cpath = package.cpath .. ";modules/lib/lib\\dyn/?.dll"
 e24 = require("e24")
 
 print(e24:help())
+
+print(e24:readADC(1), e24:readADC(2), e24:readADC(3), e24:readADC(4))
--- modules/e24/src/main.cpp
+++ modules/e24/src/main.cpp
@@ -1,7 +1,28 @@
 #include <lua5.3/lua.hpp>
+#include <fstream>
 
 static int L_help(lua_State *L) {
   lua_pushstring(L, "This module is needed  to work with a voltmeter e24");
+  return 1;
+}
+
+static int L_ReadADC(lua_State *L) {
+  int nchannel = luaL_checkinteger(L, -1);
+  double voltage;
+
+  char path[22];
+  sprintf(path, "/opt/dev/e24/adc%i.adc", nchannel);
+
+  std::fstream fs (path, std::fstream::in);
+  if(!fs.is_open()) {
+    lua_pushnil(L);
+    return 1;
+  }
+
+  fs >> voltage;
+  fs.close();
+
+  lua_pushnumber(L, voltage);
   return 1;
 }
 
@@ -11,5 +32,10 @@ extern "C" int luaopen_e24(lua_State *L) {
     lua_pushstring(L, "help");
     lua_pushcfunction(L, L_help);
     lua_rawset(L, -3);
+
+    lua_pushstring(L, "readADC");
+    lua_pushcfunction(L, L_ReadADC);
+    lua_rawset(L, -3);
+
     return 1;
 }
