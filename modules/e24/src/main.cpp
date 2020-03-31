1564c7d92304082167b3dda912340bc7634f0b58
petrolax <poa21y@gmail.com>
2020-03-31 17:31:22 +0300

    e24: Changed implementation of channels from functions in classes

--- modules/e24/src/main.cpp
+++ modules/e24/src/main.cpp
@@ -1,146 +1,155 @@
 #include <lua5.2/lua.hpp>
 #include <fstream>
 
-double ch1_begin = -5, ch1_end = 5,
-       ch2_begin = -5, ch2_end = 5,
-       ch3_begin = -5, ch3_end = 5,
-       ch4_begin = -5, ch4_end = 5;
+#ifdef _WIN32
+  #define FILE_PATH "C:/dev/e24/"
+#else
+  #define FILE_PATH "/opt/dev/e24/"
+#endif
 
-static int ch1_getVoltage(lua_State *L) {
-  double voltage;
-  std::fstream fs ("/opt/dev/e24/adc1.adc", std::fstream::in);
-  if(!fs.is_open()) {
-    lua_pushnil(L);
-    return 1;
-  }
+class channel1 {
+  private:
+    static double range;
+  public:
+    static int getVoltage(lua_State *L) {
+      double voltage;
+      std::fstream fs ((std::string)FILE_PATH + "adc1.adc", std::fstream::in);
+      if(!fs.is_open()) {
+        lua_pushnil(L);
+        return 1;
+      }
 
-  fs >> voltage;
-  fs.close();
+      fs >> voltage;
+      fs.close();
 
-  if(ch1_begin > voltage || ch1_end < voltage) {
-    lua_pushnil(L);
-    return 1;
-  }
+      if((-range/2) > voltage || (range/2) < voltage) {
+        lua_pushnil(L);
+        return 1;
+      }
 
-  lua_pushnumber(L, voltage);
-  return 1;
-}
+      lua_pushnumber(L, voltage);
+      return 1;
+    }
 
-static int ch1_setVoltScale(lua_State *L) {
-  double begin = luaL_checknumber(L, -2);
-  double end = luaL_checknumber(L, -1);
+    static int setRange(lua_State *L) {
+      double editRange = luaL_checknumber(L, -1);
 
-  if(begin < (double)-5 || end > (double)5 || begin > (double)5 || end < (double)-5) {
-    ch1_begin = -5;
-    ch1_end = 5;
-  } else {
-    ch1_begin = begin;
-    ch1_end = end;
-  }
-  return 0;
-}
+      if(editRange > 10 || editRange == (double)0) {
+        range = 10;
+      } else {
+        range = editRange;
+      }
+      return 1;
+    }
+};
 
-static int ch2_getVoltage(lua_State *L) {
-  double voltage;
-  std::fstream fs ("/opt/dev/e24/adc2.adc", std::fstream::in);
-  if(!fs.is_open()) {
-    lua_pushnil(L);
-    return 1;
-  }
+class channel2 {
+ private:
+    static double range;
+  public:
+    static int getVoltage(lua_State *L) {
+      double voltage;
+      std::fstream fs ((std::string)FILE_PATH + "adc2.adc", std::fstream::in);
+      if(!fs.is_open()) {
+        lua_pushnil(L);
+        return 1;
+      }
 
-  fs >> voltage;
-  fs.close();
+      fs >> voltage;
+      fs.close();
 
-  if(ch2_begin > voltage || ch2_end < voltage) {
-    lua_pushnil(L);
-    return 1;
-  }
+      if((-range/2) > voltage || (range/2) < voltage) {
+        lua_pushnil(L);
+        return 1;
+      }
 
-  lua_pushnumber(L, voltage);
-  return 1;
-}
+      lua_pushnumber(L, voltage);
+      return 1;
+    }
 
-static int ch2_setVoltScale(lua_State *L) {
-  double begin = luaL_checknumber(L, -2);
-  double end = luaL_checknumber(L, -1);
+    static int setRange(lua_State *L) {
+      double editRange = luaL_checknumber(L, -1);
 
-  if(begin < (double)-5 || end > (double)5 || begin > (double)5 || end < (double)-5) {
-    ch2_begin = -5;
-    ch2_end = 5;
-  } else {
-    ch2_begin = begin;
-    ch2_end = end;
-  }
-  return 0;
-}
+      if(editRange > 10 || editRange == (double)0) {
+        range = 10;
+      } else {
+        range = editRange;
+      }
+      return 0;
+    }
+};
 
-static int ch3_getVoltage(lua_State *L) {
-  double voltage;
-  std::fstream fs ("/opt/dev/e24/adc3.adc", std::fstream::in);
-  if(!fs.is_open()) {
-    lua_pushnil(L);
-    return 1;
-  }
+class channel3 {
+  private:
+    static double range;
+  public:
+    static int getVoltage(lua_State *L) {
+      double voltage;
+      std::fstream fs ((std::string)FILE_PATH + "adc3.adc", std::fstream::in);
+      if(!fs.is_open()) {
+        lua_pushnil(L);
+        return 1;
+      }
 
-  fs >> voltage;
-  fs.close();
+      fs >> voltage;
+      fs.close();
 
-  if(ch3_begin > voltage || ch3_end < voltage) {
-    lua_pushnil(L);
-    return 1;
-  }
+      if((-range/2) > voltage || (range/2) < voltage) {
+        lua_pushnil(L);
+        return 1;
+      }
 
-  lua_pushnumber(L, voltage);
-  return 1;
-}
+      lua_pushnumber(L, voltage);
+      return 1;
+    }
 
-static int ch3_setVoltScale(lua_State *L) {
-  double begin = luaL_checknumber(L, -2);
-  double end = luaL_checknumber(L, -1);
+    static int setRange(lua_State *L) {
+      double editRange = luaL_checknumber(L, -1);
 
-  if(begin < (double)-5 || end > (double)5 || begin > (double)5 || end < (double)-5) {
-    ch3_begin = -5;
-    ch3_end = 5;
-  } else {
-    ch3_begin = begin;
-    ch3_end = end;
-  }
-  return 0;
-}
+      if(editRange > 10 || editRange == (double)0) {
+        range = 10;
+      } else {
+        range = editRange;
+      }
+      return 0;
+    }
+};
 
-static int ch4_getVoltage(lua_State *L) {
-  double voltage;
-  std::fstream fs ("/opt/dev/e24/adc4.adc", std::fstream::in);
-  if(!fs.is_open()) {
-    lua_pushnil(L);
-    return 1;
-  }
+class channel4 {
+  private:
+    static double range;
+  public:
+    static int getVoltage(lua_State *L) {
+      double voltage;
+      std::fstream fs ((std::string)FILE_PATH + "adc4.adc", std::fstream::in);
+      if(!fs.is_open()) {
+        lua_pushnil(L);
+        return 1;
+      }
 
-  fs >> voltage;
-  fs.close();
+      fs >> voltage;
+      fs.close();
 
-  if(ch4_begin > voltage || ch4_end < voltage) {
-    lua_pushnil(L);
-    return 1;
-  }
+      if((-range/2) > voltage || (range/2) < voltage) {
+        lua_pushnil(L);
+        return 1;
+      }
 
-  lua_pushnumber(L, voltage);
-  return 1;
-}
+      lua_pushnumber(L, voltage);
+      return 1;
+    }
 
-static int ch4_setVoltScale(lua_State *L) {
-  double begin = luaL_checknumber(L, -2);
-  double end = luaL_checknumber(L, -1);
+    static int setRange(lua_State *L) {
+      double editRange = luaL_checknumber(L, -1);
 
-  if(begin < (double)-5 || end > (double)5 || begin > (double)5 || end < (double)-5) {
-    ch4_begin = -5;
-    ch4_end = 5;
-  } else {
-    ch4_begin = begin;
-    ch4_end = end;
-  }
-  return 0;
-}
+      if(editRange > 10 || editRange == (double)0) {
+        range = 10;
+      } else {
+        range = editRange;
+      }
+      return 0;
+    }
+};
 
 static int L_help(lua_State *L) {
   lua_pushstring(L, "This module is needed  to work with a voltmeter e24");
@@ -152,7 +161,7 @@ static int L_ReadADC(lua_State *L) {
   double voltage;
 
   char path[22];
-  sprintf(path, "/opt/dev/e24/adc%i.adc", nchannel);
+  sprintf(path, "%sadc%i.adc", FILE_PATH, nchannel);
 
   std::fstream fs (path, std::fstream::in);
   if(!fs.is_open()) {
@@ -166,6 +175,11 @@ static int L_ReadADC(lua_State *L) {
   lua_pushnumber(L, voltage);
   return 1;
 }
+
+double channel1::range{10};
+double channel2::range{10};
+double channel3::range{10};
+double channel4::range{10};
 
 extern "C" int luaopen_e24(lua_State *L) {
   lua_newtable(L);
@@ -181,44 +195,44 @@ extern "C" int luaopen_e24(lua_State *L) {
     lua_pushstring(L, "channel1");
     lua_newtable(L);
       lua_pushstring(L, "getVoltage");
-      lua_pushcfunction(L, ch1_getVoltage);
+      lua_pushcfunction(L, channel1::getVoltage);
       lua_rawset(L, -3);
 
-      lua_pushstring(L, "setVoltScale");
-      lua_pushcfunction(L, ch1_setVoltScale);
+      lua_pushstring(L, "setRange");
+      lua_pushcfunction(L, channel1::setRange);
       lua_rawset(L, -3);
     lua_rawset(L, -3);
 
     lua_pushstring(L, "channel2");
     lua_newtable(L);
       lua_pushstring(L, "getVoltage");
-      lua_pushcfunction(L, ch2_getVoltage);
+      lua_pushcfunction(L, channel2::getVoltage);
       lua_rawset(L, -3);
 
-      lua_pushstring(L, "setVoltScale");
-      lua_pushcfunction(L, ch2_setVoltScale);
+      lua_pushstring(L, "setRange");
+      lua_pushcfunction(L, channel2::setRange);
       lua_rawset(L, -3);
     lua_rawset(L, -3);
 
     lua_pushstring(L, "channel3");
     lua_newtable(L);
       lua_pushstring(L, "getVoltage");
-      lua_pushcfunction(L, ch3_getVoltage);
+      lua_pushcfunction(L, channel3::getVoltage);
       lua_rawset(L, -3);
 
-      lua_pushstring(L, "setVoltScale");
-      lua_pushcfunction(L, ch3_setVoltScale);
+      lua_pushstring(L, "setRange");
+      lua_pushcfunction(L, channel3::setRange);
       lua_rawset(L, -3);
     lua_rawset(L, -3);
 
     lua_pushstring(L, "channel4");
     lua_newtable(L);
       lua_pushstring(L, "getVoltage");
-      lua_pushcfunction(L, ch4_getVoltage);
+      lua_pushcfunction(L, channel4::getVoltage);
       lua_rawset(L, -3);
 
-      lua_pushstring(L, "setVoltScale");
-      lua_pushcfunction(L, ch4_setVoltScale);
+      lua_pushstring(L, "setRange");
+      lua_pushcfunction(L, channel4::setRange);
       lua_rawset(L, -3);
     lua_rawset(L, -3);
 
