45871191fa4abb25d2c606a606be3d0233578485
petrolax <poa21y@gmail.com>
2020-04-02 12:25:08 +0300

    Changed script of e24

--- data/script_e24.lua
+++ data/script_e24.lua
@@ -3,22 +3,8 @@ package.cpath = package.cpath .. ";modules/lib/lib\\dyn/?.dll"
 
 e24 = require("e24")
 
-print(e24:help())
-
-print(e24:readADC(1), e24:readADC(2), e24:readADC(3), e24:readADC(4))
-
-print(e24.channel1:getVoltage())
-e24.channel1:setVoltScale(1, 4)
-print(e24.channel2:getVoltage())
-e24.channel2:setVoltScale(1, 1.5)
-print(e24.channel3:getVoltage())
-e24.channel3:setVoltScale(0, 3)
-print(e24.channel4:getVoltage())
-e24.channel4:setVoltScale(-2, 3.4)
-
-print()
-
-print(e24.channel1:getVoltage())
-print(e24.channel2:getVoltage())
-print(e24.channel3:getVoltage())
-print(e24.channel4:getVoltage())
+e24:help()
+print(e24[1]:getRange())
+e24[1]:setRange(0.1)
+print(e24[1]:getRange())
+print(e24[1]:readVoltage())
