22c41e33d1a4d5da56786998c4ac6d40ad675a0e
petrolax <poa21y@gmail.com>
2020-04-26 21:16:36 +0300

    lps305: Added functions with information about the device (getModel, getVersion) and a function that returns a commands for working with the device

--- modules/lps305/src/main.cpp
+++ modules/lps305/src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <cstring>
 #include <lua5.2/lua.hpp>
 #include "lps305.h"
 
 static int L_help(lua_State *L){
-  lua_pushstring(L, "This module is needed  to work with power supply LPS-305.\nIt contains next functions:\n-- lps305:connectNewDevice(path, baudrate) - The function of connecting to a given 'path' with rate a 'baudrate'. Returns the table for working with the device.\n-- obj:setOuput(mode) - performs enable/disable outputs device\n-- obj:disconnect() - function to disconnect from the device.\n!'obj' is the variable to which the table was assigned using 'lps305: connectNewDevice'\n");
+  lua_pushstring(L, "This module is needed  to work with power supply LPS-305.\nIt contains next functions:\n-- lps305:connectNewDevice(path, baudrate) - The function of connecting to a given 'path' with rate a 'baudrate'. Returns the table for working with the device.\n-- obj:setOuput(mode) - performs enable/disable outputs device\n-- obj:getModel() - returns a string containing the model of the device\n-- obj:getVersion() - returns a string containing the version of the device\n-- obj:getDeviceHelp() - returns a string containing commands for working with the device\n-- obj:disconnect() - function to disconnect from the device.\n!'obj' is the variable to which the table was assigned using 'lps305: connectNewDevice'\n");
   return 1;
 }
 
@@ -31,6 +32,81 @@ static int L_setOutput(lua_State *L) {
   }
   lps->setOutput(mode);
   
+  return 1;
+}
+
+static int L_getModel(lua_State *L) {
+  lua_getmetatable(L, -1);
+  if(!lua_istable(L, -1)) {
+    printf("Not metatable!\n");
+    return 0;
+  }
+  
+  lua_getfield(L, -1, "_objLPS305_");
+  if(!lua_islightuserdata(L, -1)) {
+    printf("Not userdata!\n");
+    return 0;
+  }
+  LPS305 *lps  = (LPS305*)lua_touserdata(L, -1);
+  if(lps == NULL) {
+    fprintf(stderr, "Call 'connect' before using anything functions");
+    return -1;
+  }
+  char* res;
+  res = lps->getModel();
+  
+  lua_pushlstring(L, res, strlen(res));
+  free(res);
+  return 1;
+}
+
+static int L_getVersion(lua_State *L) {
+  lua_getmetatable(L, -1);
+  if(!lua_istable(L, -1)) {
+    printf("Not metatable!\n");
+    return 0;
+  }
+  
+  lua_getfield(L, -1, "_objLPS305_");
+  if(!lua_islightuserdata(L, -1)) {
+    printf("Not userdata!\n");
+    return 0;
+  }
+  LPS305 *lps  = (LPS305*)lua_touserdata(L, -1);
+  if(lps == NULL) {
+    fprintf(stderr, "Call 'connect' before using anything functions");
+    return -1;
+  }
+  char* res;
+  res = lps->getVersion();
+  
+  lua_pushlstring(L, res, strlen(res));
+  free(res);
+  return 1;
+}
+
+static int L_getDeviceHelp(lua_State *L) {
+  lua_getmetatable(L, -1);
+  if(!lua_istable(L, -1)) {
+    printf("Not metatable!\n");
+    return 0;
+  }
+  
+  lua_getfield(L, -1, "_objLPS305_");
+  if(!lua_islightuserdata(L, -1)) {
+    printf("Not userdata!\n");
+    return 0;
+  }
+  LPS305 *lps  = (LPS305*)lua_touserdata(L, -1);
+  if(lps == NULL) {
+    fprintf(stderr, "Call 'connect' before using anything functions");
+    return -1;
+  }
+  char* res;
+  res = lps->getHelp();
+  
+  lua_pushlstring(L, res, strlen(res));
+  free(res);
   return 1;
 }
 
@@ -100,6 +176,18 @@ static int L_connectNewDevice(lua_State *L) {
     lua_pushcfunction(L, L_setOutput);
     lua_rawset(L, -3);
 
+    lua_pushstring(L, "getModel");
+    lua_pushcfunction(L, L_getModel);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getVersion");
+    lua_pushcfunction(L, L_getVersion);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getDeviceHelp");
+    lua_pushcfunction(L, L_getDeviceHelp);
+    lua_rawset(L, -3);
+    
     lua_pushstring(L, "disconnect");
     lua_pushcfunction(L, L_disconnect);
     lua_rawset(L, -3);
