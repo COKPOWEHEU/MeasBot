91879d46446ab4469a8e3894b0819ddf6c67e327
COKPOWEHEU <COKPOWEHEU@yandex.ru>
2020-09-04 13:36:35 +0300

    sr5105: add functions rawSend, getMagPhase, getSens, setSens. Behaviour of measure function changed to return exact value in Si

---
+++ data/script_sr5105.lua
@@ -0,0 +1,66 @@
+--Хак с путями в винде и линуксе. Ужасная штука, но вроде работает
+package.cpath = package.cpath .. ";modules/lib/lib\\dyn/?.so"
+package.cpath = package.cpath .. ";modules/lib/lib\\dyn/?.dll"
+--package.cpath = package.cpath .. ModuleSuffix()
+--TODO эту загрузку надо как-то починить, а то больно криво смотрится
+
+function tblprint(tbl, pref, postf)
+  if pref == nil then pref="[" end
+  if postf== nil then postf="]" end
+  if type(tbl) ~= "table" then 
+    print("wrong table:", type(tbl))
+    return
+  end
+  local mt = getmetatable(tbl)
+  if mt ~= nil then
+    tblprint(mt, pref..pref, postf..postf)
+  end
+  mt = tbl
+  print("(", tbl, ")")
+  for idx,val in pairs(mt) do
+    print(pref, idx, postf, " = ", val)
+  end
+  if pref == "" or pref == nil then
+    print("-------------------------------")
+  end
+end
+
+sr5105_lib = require("sr5105")
+tblprint(sr5105_lib)
+
+sr5105 = sr5105_lib:connectNewDevice("/dev/ttyUSB0", 4800)
+if sr5105 == nil then os.exit(); end
+tblprint(sr5105)
+
+gui = require("mygui_gtk")
+gui.wnd = gui:NewWindow(800, 600, "Test gtk")
+fldX = gui.wnd:NewLabel(0,0,"")
+fldY = gui.wnd:NewLabel(0,20,"")
+fldSens = gui.wnd:NewEdit(0,40,"1")
+fldSens.prev = 1
+resSens = gui.wnd:NewLabel(200,40,"a")
+
+while gui:update() do
+  fldSens.cur = tonumber(fldSens.text)
+  if fldSens.cur ~= nil then
+    if fldSens.cur ~= fldSens.prev then
+      local s = sr5105:setSens(fldSens.cur)
+      resSens.label = "Real: " .. tostring(s)
+      fldSens.prev = fldSens.cur
+    end
+  end
+  local x,y = sr5105:getXY()
+  fldX.label = tostring(x)
+  fldY.label = tostring(y)
+end
+
+
+--print(sr5105:getXY()) --OK
+--print(sr5105:getMagPhase()) --OK
+--print(sr5105:getSens())
+--sens = sr5105:setSens(1e-1) --OK
+--print(sens)
+--print(sr5105:rawSend("SEN\r")) --OK
+--x,y = sr5105:getXY()
+--print(x, y, x*sens/1000)
+--print(sr5105:getErrors())
--- modules/sr5105/src/main.c
+++ modules/sr5105/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <lua5.2/lua.h>
 #include <lua5.2/lualib.h>
 #include <lua5.2/lauxlib.h>
@@ -13,9 +14,10 @@
 
 typedef struct sr5105{
   ttym_t tty;
+  double sens;
 }sr5105_t;
 
-void tty_puts(ttym_t tty, char *str){
+static void tty_puts(ttym_t tty, char *str){
   char *ch = str;
   char repl;
   ssize_t res;
@@ -27,7 +29,7 @@ void tty_puts(ttym_t tty, char *str){
     }
   }
 }
-void tty_gets(ttym_t tty, char *buf, size_t len){
+static void tty_gets(ttym_t tty, char *buf, size_t len){
   ssize_t res;
   ssize_t i;
   for(i=0; i<len; i++){
@@ -45,6 +47,40 @@ static const char* sr5105_id(ttym_t tty){
   tty_puts(tty, "ID\r");
   tty_gets(tty, buf, 20);
   return buf;
+}
+
+static int findCeilInArr(const double arr[], double val){
+  val *= 0.9999; //поправка на машинную точность. Сам SR5105 обладает небольшим запасом, так что небольшая перегрузка допустима
+  for(int i=0; arr[i] != FP_NAN; i++){
+    if( val <= arr[i] )return i;
+  }
+  return -1;
+}
+
+static double sr5105_setSens(ttym_t tty, double sens){
+  const double sens_arr[] = {
+     10e-6, 31.6e-6,
+    100e-6, 316e-6,
+      1e-3, 3.16e-3,
+     10e-3, 31.6e-3,
+    100e-3, 316e-3,
+    1e0,
+    FP_NAN
+  };
+  
+  char buf[20];
+  int sens_num;
+  if(sens > 0){
+    sens_num = findCeilInArr(sens_arr, sens);
+    if(sens_num < 0)return -1;
+    sprintf(buf, "SEN %i\r", sens_num);
+    tty_puts(tty, buf);
+    tty_gets(tty, buf, 19);
+  }
+  tty_puts(tty, "SEN\r");
+  tty_gets(tty, buf, 19);
+  sens_num = atoi(buf);
+  return sens_arr[sens_num];
 }
 
 #define str_append(str, app) do{memcpy(str, app, sizeof(app)); str+=sizeof(app)-1; }while(0)
@@ -71,7 +107,7 @@ static const char* sr5105_get_error(ttym_t tty){
 }
 
 static void sr5105_reset(ttym_t tty){
-  tty_puts(tty, "HPF 0; LPF 3; P 0 0; SEN 10; TC 3; XDB 1; XOF 0 0; YOF 0 0\r");
+  tty_puts(tty, "HPF 0; LPF 3; P 0 0; TC 3; XDB 1; XOF 0 0; YOF 0 0\r");
   
   char buf[1000];
   tty_gets(tty, buf, 1000);
@@ -81,35 +117,32 @@ static void sr5105_reset(ttym_t tty){
 //=================== LUA ===============================================================
 //=======================================================================================
 
-#if 1==0
-ID
-VERSION
-reset
-
-void defDelimeterControl(int n); //DD %i
-void setFreqOfHPF(int n); //HPF %i
-void setFreqOfLPF(int n); //LPF %i
-void setSens(int mode); //SEN %d
-void configRefChannel(int refPh); //P %d %d
-void configOutChannels(int statusX, int rangeX, int statusY, int rangeY); //XOF %d %d or YOF %d %d
-void configOut(int time, int slope, int DRmode); //timeconst + slope + drmode
-void runAutoPhase(); //AQN
-void runAutoOffset(); //AXO
-double readX(); //XOF
-double readY(); //YOF
-void setTimeConstant(int time); //XTC
-void setSlope(int slope); //XDB
-void setDRmode(int DRmode); //DR
-double getSens(); //SEN
-double outRefPh(); //P
-double outChannelX(); //X
-complex<double> outChannelXY(); //XY
-double outMagnitude(); //MAG
-double outSigPh(); //PHA
-double outFrequency(); //FRQ        
-#endif
-
 #define ERROR_LOG(msg) fprintf(stderr, "%s.%s at %d : %s\n", __FILE__, __func__, __LINE__, msg)
+
+sr5105_t* ReadDevice(lua_State *L){
+  lua_getmetatable(L, 1);
+    if(!lua_istable(L, -1)){
+      ERROR_LOG("Not metatable!");
+      return NULL;
+    }
+    lua_getfield(L, -1, "handle");
+      if(!lua_islightuserdata(L, -1)){
+      ERROR_LOG("Not handle\n");
+      return NULL;
+    }
+    sr5105_t *device = (sr5105_t*)lua_topointer(L, -1);
+  lua_pop(L, 2);
+  return device;
+}
+
+static int L_OnDestroy(lua_State *L){
+  sr5105_t *device = ReadDevice(L);
+  if(device == NULL)return 0;
+  
+  ttym_close(device->tty);
+  free(device);
+  return 0;
+}
 
 static int L_help(lua_State *L){
   const char helpstring[] = "Здесь могла быть ваша справка";
@@ -118,73 +151,52 @@ static int L_help(lua_State *L){
 }
 
 static int L_reset(lua_State *L){
-  lua_getmetatable(L, -1);
-    if(!lua_istable(L, -1)){
-      ERROR_LOG("Not metatable!");
-      return 0;
-    }
-    lua_getfield(L, -1, "handle");
-      if(!lua_islightuserdata(L, -1)){
-      ERROR_LOG("Not handle\n");
-      return 0;
-    }
-    sr5105_t *device = (sr5105_t*)lua_topointer(L, -1);
-  lua_pop(L, 2);
+  sr5105_t *device = ReadDevice(L);
+  if(device == NULL)return 0;
   sr5105_reset(device->tty);
   return 0;
 }
 
+static int L_rawSend(lua_State *L){
+  sr5105_t *device = ReadDevice(L);
+  if(device == NULL)return 0;
+  if(lua_gettop(L) < 2){
+    lua_settop(L, 0);
+    lua_pushnil(L);
+    return 1;
+  }
+  if(!lua_isstring(L, 2)){
+    ERROR_LOG("Input data must be 'string'");
+    lua_settop(L, 0);
+    lua_pushnil(L);
+  }
+  char *cmd = (char*)lua_tostring(L, 2);
+  char resp[101];
+  tty_puts(device->tty, cmd);
+  tty_gets(device->tty, resp, 100);
+  lua_pushstring(L, resp);
+  return 1;
+}
+
 static int L_get_id(lua_State *L){
-  lua_getmetatable(L, -1);
-    if(!lua_istable(L, -1)){
-      ERROR_LOG("Not metatable!");
-      return 0;
-    }
-    lua_getfield(L, -1, "handle");
-      if(!lua_islightuserdata(L, -1)){
-      ERROR_LOG("Not handle\n");
-      return 0;
-    }
-    sr5105_t *device = (sr5105_t*)lua_topointer(L, -1);
-    const char *str = sr5105_id(device->tty);
-    
-    lua_pop(L, 2);
-    lua_pushstring(L, str);
+  sr5105_t *device = ReadDevice(L);
+  if(device == NULL)return 0;
+  const char *str = sr5105_id(device->tty);
+  lua_pushstring(L, str);
   return 1;
 }
 
 static int L_getErrors(lua_State *L){
-  lua_getmetatable(L, -1);
-    if(!lua_istable(L, -1)){
-      ERROR_LOG("Not metatable!");
-      return 0;
-    }
-    lua_getfield(L, -1, "handle");
-      if(!lua_islightuserdata(L, -1)){
-      ERROR_LOG("Not handle\n");
-      return 0;
-    }
-    sr5105_t *device = (sr5105_t*)lua_topointer(L, -1);
-    const char *str = sr5105_get_error(device->tty);
-    
-    lua_pop(L, 2);
-    lua_pushstring(L, str);
+  sr5105_t *device = ReadDevice(L);
+  if(device == NULL)return 0;
+  const char *str = sr5105_get_error(device->tty);
+  lua_pushstring(L, str);
   return 1;
 }
 
 static int L_getVersion(lua_State *L){
-  lua_getmetatable(L, -1);
-    if(!lua_istable(L, -1)){
-      ERROR_LOG("Not metatable!");
-      return 0;
-    }
-    lua_getfield(L, -1, "handle");
-      if(!lua_islightuserdata(L, -1)){
-      ERROR_LOG("Not handle\n");
-      return 0;
-    }
-    sr5105_t *device = (sr5105_t*)lua_topointer(L, -1);
-  lua_pop(L, 2);
+  sr5105_t *device = ReadDevice(L);
+  if(device == NULL)return 0;
   char buf[21];
   tty_puts(device->tty, "VER\r");
   tty_gets(device->tty, buf, 20);
@@ -192,46 +204,58 @@ static int L_getVersion(lua_State *L){
   return 1;
 }
 
-static int L_OnDestroy(lua_State *L){
-  lua_getmetatable(L, -1);
-    if(!lua_istable(L, -1)){
-      ERROR_LOG("Not metatable!");
-      return 0;
+//if called setSens(val) then set sens and return result; else just return result
+static int L_SetSens(lua_State *L){
+  sr5105_t *device = ReadDevice(L);
+  if(device == NULL)return 0;
+  double sens = -1;
+  
+  if(lua_gettop(L) > 1){
+    if(lua_isnumber(L, -1)){
+      sens = lua_tonumber(L, -1);
+    }else{
+      ERROR_LOG("Input data must be 'string'");
+      lua_settop(L, 0);
+      lua_pushnil(L);
     }
-    lua_getfield(L, -1, "handle");
-      if(!lua_islightuserdata(L, -1)){
-      ERROR_LOG("Not handle\n");
-      return 0;
-    }
-    sr5105_t *device = (sr5105_t*)lua_topointer(L, -1);
-    ttym_close(device->tty);
-    free(device);
-  lua_pop(L, 2);
-  return 0;
+  }
+  sens = sr5105_setSens(device->tty, sens);
+  device->sens = sens;
+  lua_pushnumber(L, sens);
+  return 1;
 }
 
 static int L_GetXY(lua_State *L){
-  lua_getmetatable(L, -1);
-    if(!lua_istable(L, -1)){
-      ERROR_LOG("Not metatable!");
-      return 0;
-    }
-    lua_getfield(L, -1, "handle");
-      if(!lua_islightuserdata(L, -1)){
-      ERROR_LOG("Not handle\n");
-      return 0;
-    }
-    sr5105_t *device = (sr5105_t*)lua_topointer(L, -1);
-  lua_pop(L, 2);
-  ttym_t tty = device->tty;
+  sr5105_t *device = ReadDevice(L);
+  if(device == NULL)return 0;
     
-  tty_puts(tty, "XY\r");
+  tty_puts(device->tty, "XY\r");
   char buffer[20];
   int xval=-100500, yval=100500;
-  tty_gets(tty, buffer, 20);
-  sscanf(buffer, "%i%i", &xval, &yval);
-  lua_pushnumber(L, xval);
-  lua_pushnumber(L, yval);
+  tty_gets(device->tty, buffer, 20);
+  sscanf(buffer, " %d,%d", &xval, &yval);
+  lua_pushnumber(L, ((double)xval)*device->sens*0.001);
+  lua_pushnumber(L, ((double)yval)*device->sens*0.001);
+  return 2;
+}
+
+static int L_GetMagPhase(lua_State *L){
+  sr5105_t *device = ReadDevice(L);
+  if(device == NULL)return 0;
+  
+  int mag=-100500, phase=100500;
+  char buffer[20];
+  tty_puts(device->tty, "MAG\r");
+  tty_gets(device->tty, buffer, 19);
+  printf("mag:%s\n", buffer);
+  mag = atoi(buffer);
+  tty_puts(device->tty, "PHA\r");
+  tty_gets(device->tty, buffer, 19);
+  printf("pha:%s\n", buffer);
+  
+  phase = atoi(buffer);
+  lua_pushnumber(L, ((double)mag)*device->sens*0.001);
+  lua_pushnumber(L, ((double)phase)*0.1);
   return 2;
 }
 
@@ -272,6 +296,8 @@ static int L_connectNewDevice(lua_State *L){
   sr5105_id(device->tty);
   sr5105_get_error(device->tty);
   sr5105_reset(device->tty);
+  device->sens = 1;
+  sr5105_setSens(device->tty, device->sens);
   
   lua_newtable(L);
     lua_newtable(L);
@@ -282,6 +308,8 @@ static int L_connectNewDevice(lua_State *L){
     lua_setmetatable(L, -2);
     lua_pushcfunction(L, L_help);
     lua_setfield(L, -2, "help");
+    lua_pushcfunction(L, L_rawSend);
+    lua_setfield(L, -2, "rawSend");
     lua_pushcfunction(L, L_get_id);
     lua_setfield(L, -2, "getID");
     lua_pushcfunction(L, L_getErrors);
@@ -290,12 +318,46 @@ static int L_connectNewDevice(lua_State *L){
     lua_setfield(L, -2, "getVersion");
     lua_pushcfunction(L, L_reset);
     lua_setfield(L, -2, "reset");
+    
+    lua_pushcfunction(L, L_SetSens);
+    lua_setfield(L, -2, "setSens");
+    lua_pushcfunction(L, L_SetSens); //It is not an error, it is the same function as previous
+    lua_setfield(L, -2, "getSens");
     lua_pushcfunction(L, L_GetXY);
     lua_setfield(L, -2, "getXY");
-  //TODO TODO
-  
+    lua_pushcfunction(L, L_GetMagPhase);
+    lua_setfield(L, -2, "getMagPhase");
+    
+    //TODO: add methods
   return 1;
 }
+#if 1==0
+ID
+VERSION
+reset
+
+void defDelimeterControl(int n); //DD %i
+void setFreqOfHPF(int n); //HPF %i
+void setFreqOfLPF(int n); //LPF %i
+void setSens(int mode); //SEN %d
+void configRefChannel(int refPh); //P %d %d
+void configOutChannels(int statusX, int rangeX, int statusY, int rangeY); //XOF %d %d or YOF %d %d
+void configOut(int time, int slope, int DRmode); //timeconst + slope + drmode
+void runAutoPhase(); //AQN
+void runAutoOffset(); //AXO
+double readX(); //XOF
+double readY(); //YOF
+void setTimeConstant(int time); //XTC
+void setSlope(int slope); //XDB
+void setDRmode(int DRmode); //DR
+double getSens(); //SEN
+double outRefPh(); //P
+double outChannelX(); //X
+complex<double> outChannelXY(); //XY
+double outMagnitude(); //MAG
+double outSigPh(); //PHA
+double outFrequency(); //FRQ        
+#endif
 
 int luaopen_sr5105(lua_State *L) {
   lua_newtable(L);
