b5cdc905ddbf06cc0db960fe64c1ba77d94a8f3c
petrolax <poa21y@gmail.com>
2020-09-07 23:11:30 +0300

    sr830: Add connect and disconnect functions

--- modules/sr830/src/main.cpp
+++ modules/sr830/src/main.cpp
@@ -1,13 +1,83 @@
+#include <iostream>
 #include <lua5.2/lua.hpp>
 #include "SR830.hpp"
 
 static int L_help(lua_State *L) {
-  const char helpstring[] = "Module for working with DSP Lock-In Amplifier SR830\n";
+  const char helpstring[] =
+      "SR830\n"
+      "  help():string - return help string\n"
+      "  connectNewDevice(path, [baudrate]):table - connect to SR830. The baud rate from 300 to 19200 baud (Default is 9600)\n"
+      ;
   lua_pushstring(L, helpstring);
   return 1;
 }
 
+static int L_disconnect(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)) {
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)) {
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  sr->close();
+
+  delete sr;
+  lua_pushlightuserdata(L, nullptr);
+  lua_setfield(L, 1, "_objSR830_");
+
+  return 1;
+}
+
 static int L_connectNewDevice(lua_State *L) {
+  char *portname = nullptr;
+  int baud = 9600, checkError = 0;
+  if(lua_gettop(L) >= 2) {
+    if(lua_isstring(L, 2)) portname = (char*)lua_tostring(L, 2);
+  } else {
+    ERROR_LOG("This function must contain at least 1 parameter (port name)");
+    return 0;
+  }
+  if(lua_gettop(L) >= 3) {
+    if(lua_isnumber(L, 3)) baud = lua_tonumber(L, 3);
+  }
+
+  SR830 *sr;
+  try {
+    sr = new SR830;
+  } catch (std::bad_alloc &ba) {
+    ERROR_LOG(ba.what());
+    return 0;
+  }
+
+  checkError = sr->connect(portname, baud);
+
+  if(checkError == 0) {
+    ERROR_LOG("Port isn't open");
+    delete sr;
+    return 0;
+  }
+
+  lua_newtable(L);
+    lua_newtable(L);
+      lua_pushstring(L, "_objSR830_");
+      lua_pushlightuserdata(L, sr);
+      lua_rawset(L, -3);
+      
+      lua_pushstring(L, "_gcc");
+      lua_pushcfunction(L, L_disconnect);
+      lua_rawset(L, -3);
+    lua_setmetatable(L, -2);
 
   return 1;
 }
