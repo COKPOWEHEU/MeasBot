734bc1136fc660cf07a5c57e761233eb4af55202
petrolax <poa21y@gmail.com>
2020-09-08 17:25:43 +0300

    sr830: Add functions of output data and change help string

--- modules/sr830/src/main.cpp
+++ modules/sr830/src/main.cpp
@@ -7,6 +7,51 @@ static int L_help(lua_State *L) {
       "SR830\n"
       "  help():string - return help string\n"
       "  connectNewDevice(path, [baudrate]):table - connect to SR830. The baud rate from 300 to 19200 baud (Default is 9600)\n"
+      "  setRS232():nil - set up work with RS232 interface\n"
+      "  reset():nil - resets the SR830 to its default configurations\n"
+      "  readID():string - returns the SR830's device identification string\n"
+      "  clrRegs():nil - clears all status registers\n"
+      "  setPermStatRegs(rese, rliae, rerre, rsre):nil - setup some status registers(read manual from 5-20 to 5-23)\n"
+      "  setInConfig(inConf):nil - sets the input configuration\n"
+      "  setInGndShield(gndval):nil - sets the input shield grounding\n"
+      "  setInCoupling(coupling):nil - sets the input coupling\n"
+      "  setPhase(phase):nil - sets the reference phase shift\n"
+      "  setSens(sens):nil -  sets the sensitivity\n"
+      "  setReserveMode(mode):nil - sets the reserve mode\n"
+      "  setRefSource(mode):nil - sets the reference source\n"
+      "  setDetHarm(harm):nil - sets the detection harmonic\n"
+      "  setRefFreq(freq):nil - sets the reference frequency\n"
+      "  setAmplSinOut(volt):nil - sets the amplitude of the sine output\n"
+      "  setRefTrig(mode):nil - sets the reference trigger when using the external reference mode\n"
+      "  setNotchFiltStatus(status):nil - sets the input line notch filter status\n"
+      "  setSyncFiltStatus(status):nil - sets the synchronous filter status\n"
+      "  setTimeConst(time):nil - sets the time constan\n"
+      "  setFiltSlope(slope):nil - sets the low pass filter slope\n"
+      "  setDisplaySettings(nchannel, ndisplay, ratio):nil - selects the CH1 and CH2 displays\n"
+      "  setOutSource(nchannel, outQuan):nil - sets the front panel (CH1 and CH2) output sources\n"
+      "  setOffsetGain(nchannel, perOffset, nExpand):nil - sets the output offsets and expands\n"
+      "  setOffsetGainX(perOffset, nExpand):nil - sets the X output offsets and expands\n"
+      "  setOffsetGainY(perOffset, nExpand):nil - sets the Y output offsets and expands\n"
+      "  setAuxOutVolt(nOutput, volt):nil - sets the Aux Output voltage when the output\n"
+      "  setZeroAOV():nil - sets all(4) Aux output at 0\n"
+      "  getPhase():number - queries the reference phase shift\n"
+      "  getSens():number -  queries the sensitivity\n"
+      "  getReserveMode():number - queries the reserve mode\n"
+      "  getRefSource():bool - queries the reference source\n"
+      "  getDetHarm():number - queries the detection harmonic\n"
+      "  getRefFreq():number - queries the reference frequency\n"
+      "  getAmplSinOut():number - queries the amplitude of the sine output\n"
+      "  getRefTrig():number - queries the reference trigger when using the external reference mode\n"
+      "  getNotchFiltStatus():number - queries the input line notch filter status\n"
+      "  getSyncFiltStatus():bool - queries the synchronous filter status\n"
+      "  getTimeConst():number - queries the time constan\n"
+      "  getFiltSlope():number - queries the low pass filter slope\n"
+      "  getOffsetGain(nchannel):number,number - \n"
+      "  getAuxOutVolt(nOutput):number - queries the output offsets and expands\n"
+      "  getX():number - read the value of X\n"
+      "  getY():number - read the value of Y\n"
+      "  getR():number - read the value of R\n"
+      "  getTetta():number - read the value of tetta\n"
       ;
   lua_pushstring(L, helpstring);
   return 1;
@@ -807,9 +852,7 @@ static int L_setZeroAOV(lua_State *L) {
   return 0;
 }
 
-
-/*Template
-static int name (lua_State *L) {
+static int L_getPhase(lua_State *L) {
   lua_getmetatable(L, 1);
   if(!lua_istable(L, -1)){
     ERROR_LOG("Not metatable!");
@@ -827,9 +870,433 @@ static int name (lua_State *L) {
     ERROR_LOG("Call 'connectNewDevice' before using anything functions");
     return 0;
   }
-}
-*/
+  auto res = sr->getPhase();
 
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getSens(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getSens();
+  
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getReserveMode(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getReserveMode();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getRefSource(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getRefSource();
+
+  lua_pushboolean(L, res);
+  return 1;
+}
+
+static int L_getDetHarm(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getDetHarm();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getRefFreq(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getRefFreq();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getAmplSinOut(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getAmplSinOut();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getRefTrig(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getRefTrig();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getNotchFiltStatus(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getNotchFiltStatus();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getSyncFiltStatus(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getSyncFiltStatus();
+
+  lua_pushboolean(L, res);
+  return 1;
+}
+
+static int L_getTimeConst(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getTimeConst();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getFiltSlope(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getFiltSlope();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getOffsetGain(lua_State *L) {
+  int nchannel = 0;
+  if(lua_gettop(L) >= 2 && lua_isnumber(L, 2)) nchannel = lua_tonumber(L, 2);
+  else {
+    ERROR_LOG("This function must contain at least 1 parameter (nchannel)");
+    return 0;
+  }
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getOffsetGain(nchannel);
+
+  lua_pushnumber(L, res.real());
+  lua_pushnumber(L, res.imag());
+  return 2;
+}
+
+static int L_getAuxOutVolt(lua_State *L) {
+  int nOutput = 0;
+  if(lua_gettop(L) >= 2 && lua_isnumber(L, 2)) nOutput = lua_tonumber(L, 2);
+  else {
+    ERROR_LOG("This function must contain at least 1 parameter (nOutput)");
+    return 0;
+  }
+
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getAuxOutVolt(nOutput);
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getX(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getX();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getY(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getY();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getR(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getR();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
+
+static int L_getTetta(lua_State *L) {
+  lua_getmetatable(L, 1);
+  if(!lua_istable(L, -1)){
+    ERROR_LOG("Not metatable!");
+    return 0;
+  }
+
+  lua_getfield(L, -1, "_objSR830_");
+  if(!lua_islightuserdata(L, -1)){
+    ERROR_LOG("Not userdata!");
+    return 0;
+  }
+
+  SR830 *sr = (SR830*)lua_touserdata(L, -1);
+  if(sr == NULL) {
+    ERROR_LOG("Call 'connectNewDevice' before using anything functions");
+    return 0;
+  }
+  auto res = sr->getTetta();
+
+  lua_pushnumber(L, res);
+  return 1;
+}
 
 static int L_disconnect(lua_State *L) {
   lua_getmetatable(L, 1);
@@ -1006,6 +1473,78 @@ static int L_connectNewDevice(lua_State *L) {
     lua_pushcfunction(L, L_setZeroAOV);
     lua_rawset(L, -3);
     
+    lua_pushstring(L, "getPhase");
+    lua_pushcfunction(L, L_getPhase);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getSens");
+    lua_pushcfunction(L, L_getSens);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getReserveMode");
+    lua_pushcfunction(L, L_getReserveMode);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getRefSource");
+    lua_pushcfunction(L, L_getRefSource);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getDetHarm");
+    lua_pushcfunction(L, L_getDetHarm);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getRefFreq");
+    lua_pushcfunction(L, L_getRefFreq);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getAmplSinOut");
+    lua_pushcfunction(L, L_getAmplSinOut);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getRefTrig");
+    lua_pushcfunction(L, L_getRefTrig);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getNotchFiltStatus");
+    lua_pushcfunction(L, L_getNotchFiltStatus);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getSyncFiltStatus");
+    lua_pushcfunction(L, L_getSyncFiltStatus);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getTimeConst");
+    lua_pushcfunction(L, L_getTimeConst);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getFiltSlope");
+    lua_pushcfunction(L, L_getFiltSlope);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getOffsetGain");
+    lua_pushcfunction(L, L_getOffsetGain);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getAuxOutVolt");
+    lua_pushcfunction(L, L_getAuxOutVolt);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getX");
+    lua_pushcfunction(L, L_getX);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getY");
+    lua_pushcfunction(L, L_getY);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getR");
+    lua_pushcfunction(L, L_getR);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "getTetta");
+    lua_pushcfunction(L, L_getTetta);
+    lua_rawset(L, -3);
+
 
 
     /*Template
