7ac53b50bf1531adaaf943f678c8142c66772434
petrolax <poa21y@gmail.com>
2020-09-20 17:44:01 +0300

    sr830: delete old C++ version and change it to simpiy C version

--- makefile
+++ makefile
@@ -20,5 +20,7 @@ sr570:
 	bash -c "cd modules/sr570; make $(rarch) $(target) -j"
 sr5105:
 	bash -c "cd modules/sr5105; make $(rarch) $(target) -j"
+sr830:
+	bash -c "cd modules/sr830; make $(rarch) $(target) -j"	
 lps305:
 	bash -c "cd modules/lps305; make $(rarch) $(target) -j"
--- modules/sr830/src/ttym.c
+++ modules/sr830/src/ttym.c
@@ -113,7 +113,17 @@ ssize_t ttym_write(ttym_t tty, void *buf, size_t count){
 
 ssize_t ttym_read(ttym_t tty, void *buf, size_t count){
   if(tty == NULL)return -1;
-  return read( tty->fd, buf, count );
+  char *buff = buf;
+  size_t i = 0;
+  ssize_t res = 0;
+  while(1){
+    res = read( tty->fd, &buff[i], count-i);
+    if(res <= 0)break;
+    i += res;
+  }
+  if(i == 0)return -1;
+  return i;
+  // return read( tty->fd, buf, count );
 }
 
 int ttym_timeout(ttym_t tty, ssize_t time_ms){
